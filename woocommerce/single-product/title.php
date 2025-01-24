2d25c4c6738edb17d3abde696ad110329080dd6b
Martins Bonders <wendigotech@gmail.com>
2025-01-24 23:40:33 +0100

    Füge benutzerdefinierte PHP-Dateien, CSS-Stile und WooCommerce-Template-Dateien hinzu

---
+++ woocommerce/single-product/title.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
+<div class="gy-2 justify-content-between row">
+    <div class="col-auto">
+        <h2 class="fw-bold text-dark"><?php the_title(); ?></h2>
+    </div>
+</div>
